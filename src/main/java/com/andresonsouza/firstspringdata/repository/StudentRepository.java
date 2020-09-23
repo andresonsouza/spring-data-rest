f22125eaf18a028dabd94c6e269546cc5fc127f8
Andreson Souza <andresonjazz@gmail.com>
2020-09-23 12:31:06 -0300

    first commit

---
+++ src/main/java/com/andresonsouza/firstspringdata/repository/StudentRepository.java
@@ -0,0 +1,12 @@
+package com.andresonsouza.firstspringdata.repository;
+
+import java.util.List;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.andresonsouza.firstspringdata.entity.Student;
+
+public interface StudentRepository extends MongoRepository<Student, String> {
+
+	public List<Student> findByNameLikeIgnoreCase(String name);
+}
